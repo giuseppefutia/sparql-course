78ef0d2781708f9948c537cc7f5f764e38630fcd
Giuseppe Futia <giuseppe.futia@gmail.com>
2016-05-17 11:13:42 +0200

    Update bad indexes

--- lab/referata/bot-media-wiki.js
+++ lab/referata/bot-media-wiki.js
@@ -36,39 +36,39 @@ var loadCSVinMW = function () {
         console.log("*** CONTENT ***");
         console.log();
         var content = "{{Contributo in RIVISTA" +
-        "|Id=" + csvData[1][0] +
-        "|Titolo=" + csvData[1][1] +
-        "|Anno=" + csvData[1][2] +
-        "|Formato=" + csvData[1][3] +
-        "|Autori=" + csvData[1][4] +
-        "|Affiliazione autori NON CNR=" + csvData[1][5] +
-        "|Lingua prodotto=" + csvData[1][6] +
-        "|Abstract=" + csvData[1][7] +
-        "|Lingua Abstract=" + csvData[1][8] +
-        "|Altro Abstract=" + csvData[1][9] +
-        "|Lingua Abstract=" + csvData[1][10] +
-        "|Pagine DA=" + csvData[1][11] +
-        "|Pagine A=" + csvData[1][12] +
-        "|Rivista=" + csvData[1][13] +
-        "|Volume=" + csvData[1][14] +
-        "|Fascicolo=" + csvData[1][15] +
-        "|DOI=" + csvData[1][16] +
-        "|Referee=" + csvData[1][17] +
-        "|Indicizzato da=" + csvData[1][18] +
-        "|Parole chiave=" + csvData[1][19] +
-        "|URL=" + csvData[1][20] +
-        "|Stato=" + csvData[1][21] +
-        "|Indicizzato DA=" + csvData[1][22] +
-        "|Parole chiave=" + csvData[1][23] +
-        "|URL=" + csvData[1][24] +
-        "|Sede=" + csvData[1][25] +
-        "|Progetto=" + csvData[1][26] +
-        "|Modulo=" + csvData[1][27] +
-        "|Permessi=" + csvData[1][28] +
+        "|Id=" + csvData[i][0] +
+        "|Titolo=" + csvData[i][1] +
+        "|Anno=" + csvData[i][2] +
+        "|Formato=" + csvData[i][3] +
+        "|Autori=" + csvData[i][4] +
+        "|Affiliazione autori NON CNR=" + csvData[i][5] +
+        "|Lingua prodotto=" + csvData[i][6] +
+        "|Abstract=" + csvData[i][7] +
+        "|Lingua Abstract=" + csvData[i][8] +
+        "|Altro Abstract=" + csvData[i][9] +
+        "|Lingua Abstract=" + csvData[i][10] +
+        "|Pagine DA=" + csvData[i][11] +
+        "|Pagine A=" + csvData[i][12] +
+        "|Rivista=" + csvData[i][13] +
+        "|Volume=" + csvData[i][14] +
+        "|Fascicolo=" + csvData[i][15] +
+        "|DOI=" + csvData[i][16] +
+        "|Referee=" + csvData[i][17] +
+        "|Indicizzato da=" + csvData[i][18] +
+        "|Parole chiave=" + csvData[i][19] +
+        "|URL=" + csvData[i][20] +
+        "|Stato=" + csvData[i][21] +
+        "|Indicizzato DA=" + csvData[i][22] +
+        "|Parole chiave=" + csvData[i][23] +
+        "|URL=" + csvData[i][24] +
+        "|Sede=" + csvData[i][25] +
+        "|Progetto=" + csvData[i][26] +
+        "|Modulo=" + csvData[i][27] +
+        "|Permessi=" + csvData[i][28] +
         "}}";
         console.log(content);
 
-        client.edit(csvData[1][0], content, "edit journal " + csvData[1][0], true, function (err, data) {
+        client.edit(csvData[i][0], content, "edit journal " + csvData[i][0], true, function (err, data) {
           console.log("*** EDIT A PAGE ***");
           if (err) {
             console.error(err);
